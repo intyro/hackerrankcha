eaad835f83a9b0d4d08db665c0949088fda954ca
Jokes7 <moath.khozyem@yahoo.com>
2016-01-08 23:14:53 +0200

    init

---
+++ day1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Primitive : Double\n";
+    cout << "Primitive : Character\n";
+    cout << "Primitive : Boolean\n";
+    cout << "Primitive : Integer\n";
+    cout << "Referenced : String\n";
+    cout << "Primitive : Boolean\n";
+    cout << "Primitive : Double\n";
+    cout << "Primitive : Character\n";
+    cout << "Referenced : String\n";
+    return 0;
+}
